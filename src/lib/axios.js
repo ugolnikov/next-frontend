99e56db311ef052e1ee8727444811e6799ca57e5
ugolnikov <hacker.2282005@mail.ru>
2024-11-29 13:01:31 +0500

    Update axios.js

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -4,6 +4,7 @@ const axios = Axios.create({
     baseURL: process.env.NEXT_PUBLIC_BACKEND_URL || 'https://next-backend-production-686d.up.railway.app',
     headers: {
         'X-Requested-With': 'XMLHttpRequest',
+        'X-CSRF-TOKEN': document.querySelector('meta[name="csrf-token"]').getAttribute('content'), 
     },
     withCredentials: true,
     withXSRFToken: true
